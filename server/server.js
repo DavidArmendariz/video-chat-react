f0839c70b2eb2ae18719362c2e501ca04feda524
David Armendariz <darmendariz1998@outlook.com>
2021-05-01 21:03:25 -0500

    Continuing with restructure

--- server/server.js
+++ server/server.js
@@ -1,37 +1,43 @@
-const express = require("express");
+const express = require('express');
+const http = require('http');
+const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+
 const app = express();
-const server = require("http").Server(app);
-const { v4: uuidv4 } = require("uuid");
-app.set("view engine", "ejs");
-const io = require("socket.io")(server, {
+
+app.set('view engine', 'ejs');
+
+const io = require('socket.io')(server, {
   cors: {
-    origin: '*'
-  }
+    origin: '*',
+  },
 });
-const { ExpressPeerServer } = require("peer");
+
 const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
 
-app.use("/peerjs", peerServer);
-app.use(express.static("public"));
+app.use('/peerjs', peerServer);
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
 
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
 });
 
-io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId, userName) => {
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId, userName) => {
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
-    socket.on("message", (message) => {
-      io.to(roomId).emit("createMessage", message, userName);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    socket.on('message', (message) => {
+      io.to(roomId).emit('createMessage', message, userName);
     });
   });
 });
+
+const server = http.Server(app);
 
 server.listen(process.env.PORT || 3030);
